c580aa5db759f6fd3a2413cc7e21251dfa438b1a
Sakshi Jain <sugareyed@gmail.com>
2018-07-19 16:44:53 +0530

    Small OCR Example

---
+++ src/main/java/com/example/sakshi/wordfinder/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.sakshi.wordfinder;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.googlecode.tesseract.android.TessBaseAPI;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static android.icu.lang.UCharacter.toLowerCase;
+
+public class MainActivity extends AppCompatActivity {
+
+    Bitmap image;
+    private TessBaseAPI mTess;
+    String datapath = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //init image
+        image = BitmapFactory.decodeResource(getResources(), R.drawable.test_image);
+
+        //initialize Tesseract API
+        String language = "eng";
+        datapath = getFilesDir()+ "/tesseract/";
+        mTess = new TessBaseAPI();
+
+        checkFile(new File(datapath + "tessdata/"));
+
+        mTess.init(datapath, language);
+    }
+
+    public void processImage(View view){
+        String OCRresult = null;
+        String search=null;
+        String searchword=null;
+        EditText srch = (EditText) findViewById(R.id.editText);
+        search= String.valueOf(srch.getText());
+        mTess.setImage(image);
+        OCRresult = mTess.getUTF8Text();
+        searchword=toLowerCase(search);
+        String OCR=toLowerCase(OCRresult);
+        TextView OCRTextView = (TextView) findViewById(R.id.OCRTextView);
+        int i = OCR.indexOf(searchword);
+
+        if(i!=-1){
+        OCRresult = OCRresult + "\nThe word \"" + search + "\" is present at index:\n";
+            while(i >= 0) {
+                OCRresult = OCRresult+i+"\n";
+                i = OCR.indexOf(searchword, i+1);
+            }
+        }else {
+                OCRresult = OCRresult + "\nCan't find " + search;
+            }
+
+
+        OCRTextView.setText(OCRresult);
+    }
+
+    private void checkFile(File dir) {
+        if (!dir.exists()&& dir.mkdirs()){
+            copyFiles();
+        }
+        if(dir.exists()) {
+            String datafilepath = datapath+ "/tessdata/eng.traineddata";
+            File datafile = new File(datafilepath);
+
+            if (!datafile.exists()) {
+                copyFiles();
+            }
+        }
+    }
+
+    private void copyFiles() {
+        try {
+            String filepath = datapath + "/tessdata/eng.traineddata";
+            AssetManager assetManager = getAssets();
+
+            InputStream instream = assetManager.open("tessdata/eng.traineddata");
+            OutputStream outstream = new FileOutputStream(filepath);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = instream.read(buffer)) != -1) {
+                outstream.write(buffer, 0, read);
+            }
+
+
+            outstream.flush();
+            outstream.close();
+            instream.close();
+
+            File file = new File(filepath);
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
